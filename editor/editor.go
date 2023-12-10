c036c1d878d18aad6230d1d9748f42088d2d7866
Robby <h0rv@users.noreply.github.com>
2023-12-09 22:32:41 -0500

    Add ability to pass args to editor CMD

--- editor/editor.go
+++ editor/editor.go
@@ -11,12 +11,13 @@ const defaultEditor = "nano"
 
 // Cmd returns a *exec.Cmd editing the given path with $EDITOR or nano if no
 // $EDITOR is set.
-func Cmd(app, path string) (*exec.Cmd, error) {
+func Cmd(app, path string, args ...string) (*exec.Cmd, error) {
 	if os.Getenv("SNAP_REVISION") != "" {
 		return nil, fmt.Errorf("Did you install with Snap? %[1]s is sandboxed and unable to open an editor. Please install %[1]s with Go or another package manager to enable editing.", app)
 	}
-	editor, args := getEditor()
-	return exec.Command(editor, append(args, path)...), nil
+	editor, editorArgs := getEditor()
+	args = append(append(args, editorArgs...), path)
+	return exec.Command(editor, args...), nil
 }
 
 func getEditor() (string, []string) {
