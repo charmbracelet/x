6616ed4293507b0fb25aa0ba7fbad4780d8facef
Robby <h0rv@users.noreply.github.com>
2023-12-13 15:59:41 -0500

    Add optional line number argument

--- editor/editor.go
+++ editor/editor.go
@@ -9,15 +9,46 @@ import (
 
 const defaultEditor = "nano"
 
+// Get editors that have support for the `+[line number]` flag
+// to edit the file at the current line that is in the pager
+func getEditorsWithLineNumberSupport() []string {
+	return []string{
+		"vi",
+		"vim",
+		"nvim",
+		"nano",
+	}
+}
+
+func hasLineNumberSupport(editor string) bool {
+	for _, supportedEditor := range getEditorsWithLineNumberSupport() {
+		if editor == supportedEditor {
+			return true
+		}
+	}
+    return false
+}
+
 // Cmd returns a *exec.Cmd editing the given path with $EDITOR or nano if no
 // $EDITOR is set.
-func Cmd(app, path string, args ...string) (*exec.Cmd, error) {
+func Cmd(app, path string, lineNumber_optional ...uint) (*exec.Cmd, error) {
 	if os.Getenv("SNAP_REVISION") != "" {
 		return nil, fmt.Errorf("Did you install with Snap? %[1]s is sandboxed and unable to open an editor. Please install %[1]s with Go or another package manager to enable editing.", app)
 	}
-	editor, editorArgs := getEditor()
-	args = append(append(args, editorArgs...), path)
-	return exec.Command(editor, args...), nil
+
+	editor, args := getEditor()
+
+	// Add line number to open the editor at if provided and a supported editor is being used
+	if len(lineNumber_optional) == 1 && hasLineNumberSupport(editor) {
+		lineNumber := lineNumber_optional[0]
+
+		lineNumberArg := fmt.Sprintf("+%d", lineNumber)
+		// Insert line position arg before file name and other flags (required for nano)
+		args = append([]string{lineNumberArg}, args...)
+
+	}
+
+	return exec.Command(editor, append(args, path)...), nil
 }
 
 func getEditor() (string, []string) {
