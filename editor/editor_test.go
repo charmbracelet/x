ca071ee8497cca5dd165b9a19c97211caa720539
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-12-15 14:03:07 -0300

    test: fix option name
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- editor/editor_test.go
+++ editor/editor_test.go
@@ -35,7 +35,7 @@ func TestEditor(t *testing.T) {
 		} {
 			t.Run(k, func(t *testing.T) {
 				t.Setenv("EDITOR", k)
-				cmd, _ := Cmd("X", "README.md", AtLine(12))
+				cmd, _ := Cmd("X", "README.md", OpenAtLine(12))
 				got := cmd.Args
 				if !reflect.DeepEqual(got, v) {
 					t.Fatalf("expected %v; got %v", v, got)
