e04e0c68c0a9ef2bc7c1a910c8e8235c235fb9d5
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-02-20 14:42:58 -0500

    feat(input): parse APC & DCS control codes

--- exp/term/input/ansi/driver.go
+++ exp/term/input/ansi/driver.go
@@ -192,11 +192,16 @@ func (d *driver) peekInput() (int, []input.Event, error) {
 				d.handleSeq(d.parseSs3, i, p, alt, &i, &ev)
 				continue
 			case 'P': // Esc-prefixed DCS
+				d.handleSeq(d.parseDcs, i, p, alt, &i, &ev)
+				continue
 			case '[': // Esc-prefixed CSI
 				d.handleSeq(d.parseCsi, i, p, alt, &i, &ev)
 				continue
 			case ']': // Esc-prefixed OSC
 				d.handleSeq(d.parseOsc, i, p, alt, &i, &ev)
+				continue
+			case '_': // Esc-prefixed APC
+				d.handleSeq(d.parseApc, i, p, alt, &i, &ev)
 				continue
 			default:
 				alt = true
@@ -209,11 +214,15 @@ func (d *driver) peekInput() (int, []input.Event, error) {
 			d.handleSeq(d.parseSs3, i, p, alt, &i, &ev)
 			continue
 		case ansi.DCS:
+			d.handleSeq(d.parseDcs, i, p, alt, &i, &ev)
 		case ansi.CSI:
 			d.handleSeq(d.parseCsi, i, p, alt, &i, &ev)
 			continue
 		case ansi.OSC:
 			d.handleSeq(d.parseOsc, i, p, alt, &i, &ev)
+			continue
+		case ansi.APC:
+			d.handleSeq(d.parseApc, i, p, alt, &i, &ev)
 			continue
 		default:
 			// Unknown sequence
@@ -286,16 +295,27 @@ func (d *driver) parseCsi(i int, p []byte, alt bool) (int, input.Event) {
 	}
 	// Scan final byte in the range 0x40-0x7E
 	if i >= len(p) || p[i] < 0x40 || p[i] > 0x7E {
+		// XXX: Some terminals like URxvt send invalid CSI sequences on key
+		// events such as shift modified keys (\x1b [ <func> $). We try to
+		// lookup the sequence in the table and return it as a key event if it
+		// exists. Otherwise, we report an unknown event.
+		var e input.Event = input.UnknownEvent(seq)
 		if key, ok := d.table[seq]; ok {
 			if alt {
 				key.Mod |= input.Alt
 			}
-			return len(seq), key
+			e = key
 		}
-		return len(seq), input.UnknownEvent(seq)
+		return len(seq), e
 	}
 
+	// Add the final byte
 	seq += string(p[i])
+	k, ok := d.table[seq]
+	if ok {
+		return len(seq), k
+	}
+
 	csi := ansi.CsiSequence(seq)
 	initial := csi.Initial()
 	cmd := csi.Command()
@@ -304,6 +324,7 @@ func (d *driver) parseCsi(i int, p []byte, alt bool) (int, input.Event) {
 		// Handle X10 mouse
 		return len(seq) + 3, parseX10MouseEvent(append([]byte(seq), p[i+1:i+3]...))
 	case initial == '<' && (cmd == 'm' || cmd == 'M'):
+		// Handle SGR mouse
 		return len(seq), parseSGRMouseEvent([]byte(seq))
 	case initial == 0 && cmd == 'u':
 		// Kitty keyboard protocol
@@ -353,11 +374,6 @@ func (d *driver) parseCsi(i int, p []byte, alt bool) (int, input.Event) {
 		return len(seq), key
 	}
 
-	k, ok := d.table[seq]
-	if ok {
-		return len(seq), k
-	}
-
 	return len(seq), input.UnknownEvent(seq)
 }
 
@@ -378,16 +394,18 @@ func (d *driver) parseSs3(i int, p []byte, alt bool) (int, input.Event) {
 	// A GL character is a single byte in the range 0x21-0x7E
 	// See https://vt100.net/docs/vt220-rm/chapter2.html#S2.3.2
 	if i >= len(p) || p[i] < 0x21 || p[i] > 0x7E {
+		var e input.Event = input.UnknownEvent(seq)
 		if key, ok := d.table[seq]; ok {
 			if alt {
 				key.Mod |= input.Alt
 			}
-			return len(seq), key
+			e = key
 		}
-		return len(seq), input.UnknownEvent(seq)
+		return len(seq), e
 	}
-	seq += string(p[i])
 
+	// Add the GL character
+	seq += string(p[i])
 	k, ok := d.table[seq]
 	if ok {
 		if alt {
@@ -438,4 +456,49 @@ func (d *driver) parseOsc(i int, p []byte, _ bool) (int, input.Event) {
 	}
 
 	return len(seq), input.UnknownEvent(seq)
+}
+
+// parseCtrl parses a control sequence that gets terminated by a ST character.
+func (d *driver) parseCtrl(intro8, intro7 byte) func(int, []byte, bool) (int, input.Event) {
+	return func(i int, p []byte, _ bool) (int, input.Event) {
+		var seq string
+		if p[i] == intro8 || p[i] == ansi.ESC {
+			seq += string(p[i])
+			i++
+		}
+		if i < len(p) && p[i-1] == ansi.ESC && p[i] == intro7 {
+			seq += string(p[i])
+			i++
+		}
+
+		// Scan control sequence
+		// Most common control sequence is terminated by a ST character
+		// ST is a 7-bit string terminator character is (ESC \)
+		for ; i < len(p) && p[i] != ansi.ST && p[i] != ansi.ESC; i++ {
+			seq += string(p[i])
+		}
+
+		if i >= len(p) {
+			return len(seq), input.UnknownEvent(seq)
+		}
+		seq += string(p[i])
+
+		// Check 7-bit ST (string terminator) character
+		if len(p) > i+1 && p[i] == ansi.ESC && p[i+1] == '\\' {
+			i++
+			seq += string(p[i])
+		}
+
+		return len(seq), input.UnknownEvent(seq)
+	}
+}
+
+func (d *driver) parseDcs(i int, p []byte, alt bool) (int, input.Event) {
+	// DCS sequences are introduced by DCS (0x90) or ESC P (0x1b 0x50)
+	return d.parseCtrl(ansi.DCS, 'P')(i, p, alt)
+}
+
+func (d *driver) parseApc(i int, p []byte, alt bool) (int, input.Event) {
+	// APC sequences are introduced by APC (0x9f) or ESC _ (0x1b 0x5f)
+	return d.parseCtrl(ansi.APC, '_')(i, p, alt)
 }
