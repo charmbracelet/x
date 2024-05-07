acfa9642dcb195be611e0c0987f01d9ee4242cb5
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-05-07 11:31:27 -0400

    feat(term): tidy pty

--- exp/term/xpty/pty_unix.go
+++ exp/term/xpty/pty_unix.go
@@ -8,36 +8,36 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-// resize implements XPTY.
-func (p *Pty) resize(width int, height int) (err error) {
-	conn, err := p.master.SyscallConn()
-	if err != nil {
-		return err
-	}
-
-	return conn.Control(func(fd uintptr) {
-		err = termios.SetWinsize(int(fd), &unix.Winsize{
-			Row: uint16(height),
-			Col: uint16(width),
+// setWinsize sets window size for the PTY.
+func (p *UnixPty) setWinsize(width, height, x, y int) error {
+	var rErr error
+	if err := p.Control(func(fd uintptr) {
+		rErr = termios.SetWinsize(int(fd), &unix.Winsize{
+			Row:    uint16(height),
+			Col:    uint16(width),
+			Xpixel: uint16(x),
+			Ypixel: uint16(y),
 		})
-	})
+	}); err != nil {
+		rErr = err
+	}
+	return rErr
 }
 
 // size returns the size of the PTY.
-func (p *Pty) size() (width, height int, err error) {
-	conn, err := p.master.SyscallConn()
-	if err != nil {
-		return 0, 0, err
-	}
-
-	err = conn.Control(func(fd uintptr) {
+func (p *UnixPty) size() (width, height int, err error) {
+	var rErr error
+	if err := p.Control(func(fd uintptr) {
 		ws, err := termios.GetWinsize(int(fd))
 		if err != nil {
+			rErr = err
 			return
 		}
 		width = int(ws.Col)
 		height = int(ws.Row)
-	})
+	}); err != nil {
+		rErr = err
+	}
 
-	return
+	return width, height, rErr
 }
