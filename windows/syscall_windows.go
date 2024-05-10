e174fa0946b7007415c1235b69ea535fdecaf429
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-05-10 11:46:20 -0400

    fix(windows): api methods don't return errors

--- windows/syscall_windows.go
+++ windows/syscall_windows.go
@@ -6,5 +6,5 @@ var NewLazySystemDLL = windows.NewLazySystemDLL
 
 type Handle = windows.Handle
 
-//sys GetKeyboardLayout(threadId uint32) (hkl Handle, err error) = user32.GetKeyboardLayout
-//sys ToUnicodeEx(vkey uint32, scancode uint32, keystate *byte, pwszBuff *uint16, cchBuff int32, flags uint32, hkl Handle) (ret int32, err error) = user32.ToUnicodeEx
+//sys GetKeyboardLayout(threadId uint32) (hkl Handle) = user32.GetKeyboardLayout
+//sys ToUnicodeEx(vkey uint32, scancode uint32, keystate *byte, pwszBuff *uint16, cchBuff int32, flags uint32, hkl Handle) (ret int32) = user32.ToUnicodeEx
