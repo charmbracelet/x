4a5c70c59c2d4f5982892f621ec7943a1ea0161e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-08-08 22:50:14 -0300

    feat(editor): open at the end of the line

--- editor/editor_test.go
+++ editor/editor_test.go
@@ -44,6 +44,46 @@ func TestEditor(t *testing.T) {
 		}
 	})
 
+	t.Run("with end of line", func(t *testing.T) {
+		for k, v := range map[string][]string{
+			"":             {"nano", filename},
+			"nvim":         {"nvim", "+norm! $", filename},
+			"vim":          {"vim", "+norm! $", filename},
+			"vscode --foo": {"vscode", "--foo", filename},
+			"nvim -a -b":   {"nvim", "-a", "-b", "+norm! $", filename},
+			"code --foo":   {"code", "--foo", filename},
+		} {
+			t.Run(k, func(t *testing.T) {
+				t.Setenv("EDITOR", k)
+				cmd, _ := Cmd("X", "README.md", OpenAtEndOfLine)
+				got := cmd.Args
+				if !reflect.DeepEqual(got, v) {
+					t.Fatalf("expected %v; got %v", v, got)
+				}
+			})
+		}
+	})
+
+	t.Run("with line and end of line", func(t *testing.T) {
+		for k, v := range map[string][]string{
+			"":             {"nano", "+3", filename},
+			"nvim":         {"nvim", "+norm! $", "+3", filename},
+			"vim":          {"vim", "+norm! $", "+3", filename},
+			"vscode --foo": {"vscode", "--foo", filename},
+			"nvim -a -b":   {"nvim", "-a", "-b", "+norm! $", "+3", filename},
+			"code --foo":   {"code", "--foo", "--goto", filename + ":3"},
+		} {
+			t.Run(k, func(t *testing.T) {
+				t.Setenv("EDITOR", k)
+				cmd, _ := Cmd("X", "README.md", OpenAtEndOfLine, OpenAtLine(3))
+				got := cmd.Args
+				if !reflect.DeepEqual(got, v) {
+					t.Fatalf("expected %v; got %v", v, got)
+				}
+			})
+		}
+	})
+
 	t.Run("inside snap", func(t *testing.T) {
 		t.Setenv("SNAP_REVISION", "10")
 		got, err := Cmd("X", "foo")
