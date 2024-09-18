935070799beaab3128e5449ae615df1585ee2515
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-09-18 11:58:59 -0400

    feat: add teatest/v2 to use bubbletea/v2 (#197)
    * feat: add teatest/v2 to use bubbletea@v2-exp
    
    Will keep this open until we push a bubbletea/v2 release
    
    * feat(ci): add teatest-v2 workflow
    
    * fix: use bubbletea/v2

---
+++ exp/teatest/v2/send_test.go
@@ -0,0 +1,87 @@
+package teatest_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea/v2"
+	"github.com/charmbracelet/x/exp/teatest/v2"
+)
+
+func TestAppSendToOtherProgram(t *testing.T) {
+	m1 := &connectedModel{
+		name: "m1",
+	}
+	m2 := &connectedModel{
+		name: "m2",
+	}
+
+	tm1 := teatest.NewTestModel(t, m1, teatest.WithInitialTermSize(70, 30))
+	t.Cleanup(func() {
+		if err := tm1.Quit(); err != nil {
+			t.Fatal(err)
+		}
+	})
+	tm2 := teatest.NewTestModel(t, m2, teatest.WithInitialTermSize(70, 30))
+	t.Cleanup(func() {
+		if err := tm2.Quit(); err != nil {
+			t.Fatal(err)
+		}
+	})
+	m1.programs = append(m1.programs, tm2)
+	m2.programs = append(m2.programs, tm1)
+
+	tm1.Type("pp")
+	tm2.Type("pppp")
+
+	tm1.Type("q")
+	tm2.Type("q")
+
+	out1 := readBts(t, tm1.FinalOutput(t, teatest.WithFinalTimeout(time.Second)))
+	out2 := readBts(t, tm2.FinalOutput(t, teatest.WithFinalTimeout(time.Second)))
+
+	if string(out1) != string(out2) {
+		t.Errorf("output of both models should be the same, got:\n%v\nand:\n%v\n", string(out1), string(out2))
+	}
+
+	teatest.RequireEqualOutput(t, out1)
+}
+
+type connectedModel struct {
+	name     string
+	programs []interface{ Send(tea.Msg) }
+	msgs     []string
+}
+
+type ping string
+
+func (m *connectedModel) Init() (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m *connectedModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "p":
+			send := ping("from " + m.name)
+			m.msgs = append(m.msgs, string(send))
+			for _, p := range m.programs {
+				p.Send(send)
+			}
+			fmt.Printf("sent ping %q to others\n", send)
+		case "q":
+			return m, tea.Quit
+		}
+	case ping:
+		fmt.Printf("rcvd ping %q on %s\n", msg, m.name)
+		m.msgs = append(m.msgs, string(msg))
+	}
+	return m, nil
+}
+
+func (m *connectedModel) View() string {
+	return "All pings:\n" + strings.Join(m.msgs, "\n")
+}
