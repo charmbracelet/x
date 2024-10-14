7be92e3b9b2831d11dea6b190feb018bdcc15b16
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-10-13 20:03:10 -0400

    refactor: simplify implementation and move code
    We're moving this code to be part of the bubbletea cell renderer

--- cellbuf/buffer.go
+++ cellbuf/buffer.go
@@ -2,45 +2,8 @@ package cellbuf
 
 // Buffer is a 2D grid of cells representing a screen or terminal.
 type Buffer struct {
-	cells  []Cell
-	width  int
-	method WidthMethod // Defaults to WcWidth
-}
-
-// NewBuffer creates a new Buffer with the given width and height.
-func NewBuffer(width int, method WidthMethod) *Buffer {
-	b := &Buffer{
-		cells:  make([]Cell, width),
-		width:  width,
-		method: method,
-	}
-	return b
-}
-
-// SetMethod sets the width method used by the buffer.
-func (b *Buffer) SetMethod(method WidthMethod) {
-	b.method = method
-}
-
-// Method returns the width method used by the buffer.
-func (b *Buffer) Method() WidthMethod {
-	return b.method
-}
-
-// Equal returns true if the buffer is equal to the other buffer.
-func (b *Buffer) Equal(o *Buffer) bool {
-	if b.width != o.width {
-		return false
-	}
-	if len(b.cells) != len(o.cells) {
-		return false
-	}
-	for i := range b.cells {
-		if !b.cells[i].Equal(o.cells[i]) {
-			return false
-		}
-	}
-	return true
+	cells []Cell
+	width int
 }
 
 // Width returns the width of the buffer.
@@ -50,13 +13,10 @@ func (b *Buffer) Width() int {
 
 // Height returns the height of the buffer.
 func (b *Buffer) Height() int {
+	if b.width == 0 {
+		return 0
+	}
 	return len(b.cells) / b.width
-}
-
-// Size returns the width and height of the buffer.
-func (b *Buffer) Size() (width, height int) {
-	height = len(b.cells) / b.width
-	return b.width, height
 }
 
 // At returns the cell at the given x, y position.
@@ -75,21 +35,8 @@ func (b *Buffer) At(x, y int) (Cell, error) {
 	return b.cells[idx], nil
 }
 
-// Fill fills the buffer with the given style and rune.
-func (b *Buffer) Fill(c Cell) {
-	if b.width == 0 {
-		return
-	}
-	height := len(b.cells) / b.width
-	for j := 0; j < height; j++ {
-		for i := 0; i < b.width; i++ {
-			b.Set(i, j, c) //nolint:errcheck
-		}
-	}
-}
-
 // Set sets the cell at the given x, y position.
-func (b *Buffer) Set(x, y int, c Cell) {
+func (b *Buffer) Set(x, y int, c Cell) (v bool) {
 	if b.width == 0 {
 		return
 	}
@@ -103,31 +50,32 @@ func (b *Buffer) Set(x, y int, c Cell) {
 	}
 
 	b.cells[idx] = c
-}
-
-// SetFunc sets the cell at the given x, y position using a function.
-func (b *Buffer) SetFunc(x, y int, f func(c Cell) Cell) {
-	c, err := b.At(x, y)
-	if err != nil {
-		return
-	}
-	b.Set(x, y, f(c))
-}
-
-// lastInLine returns true if the cell is the last non-space cell in the line.
-func (b *Buffer) lastInLine(x, y int) bool {
-	for i := x + 1; i < b.width; i++ {
-		if cell, err := b.At(i, y); err == nil && !cell.Equal(spaceCell) {
-			return false
-		}
-	}
 	return true
 }
 
 // Clone returns a deep copy of the buffer.
 func (b *Buffer) Clone() *Buffer {
-	clone := NewBuffer(b.width, b.method)
+	var clone Buffer
+	clone.width = b.width
 	clone.cells = make([]Cell, len(b.cells))
 	copy(clone.cells, b.cells)
-	return clone
+	return &clone
+}
+
+// Resize resizes the buffer to the given width and height. It grows the buffer
+// if necessary and fills the new cells with space cells. Otherwise, it
+// truncates the buffer.
+func (b *Buffer) Resize(width, height int) {
+	b.width = width
+	if area := width * height; len(b.cells) < area {
+		ln := len(b.cells)
+		b.cells = append(b.cells, make([]Cell, area-ln)...)
+		// Fill the buffer with space cells
+		for i := ln; i < area; i++ {
+			b.cells[i] = spaceCell
+		}
+	} else if len(b.cells) > area {
+		// Truncate the buffer if necessary
+		b.cells = b.cells[:area]
+	}
 }
