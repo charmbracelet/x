031d6fb4dd89e8ff0c5d4d5cf4dc43ca51df79f8
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-11-14 20:20:20 -0500

    fix(vt): properly handle OSC 2, 10, 11, 12, 110, 111, 112

--- vt/osc.go
+++ vt/osc.go
@@ -14,7 +14,7 @@ import (
 // handleOsc handles an OSC escape sequence.
 func (t *Terminal) handleOsc(seq ansi.OscSequence) {
 	switch cmd := t.parser.Cmd(); cmd {
-	case 0, 1, 3, 10, 11, 12:
+	case 0, 1, 2:
 		parts := bytes.Split(t.parser.Data(), []byte{';'})
 		if len(parts) != 2 {
 			// Invalid, ignore
@@ -24,56 +24,78 @@ func (t *Terminal) handleOsc(seq ansi.OscSequence) {
 		case 0: // Set window title and icon name
 			name := string(parts[1])
 			t.iconName, t.title = name, name
+			if t.Title != nil {
+				t.Title(name)
+			}
+			if t.IconName != nil {
+				t.IconName(name)
+			}
 		case 1: // Set icon name
 			name := string(parts[1])
 			t.iconName = name
+			if t.IconName != nil {
+				t.IconName(name)
+			}
 		case 2: // Set window title
 			name := string(parts[1])
 			t.title = name
-		case 10, 11, 12, 110, 111, 112:
-			var setCol func(color.Color)
-			var col color.Color
-
-			switch cmd {
-			case 10, 11, 12:
-				if s := string(parts[1]); s == "?" {
-					switch cmd {
-					case 10:
-						col = t.ForegroundColor()
-					case 11:
-						col = t.BackgroundColor()
-					case 12:
-						col = t.CursorColor()
-					}
-
-					t.buf.WriteString(encodeOscColor(cmd.Command(), col))
-				} else {
-					col := xParseColor(string(parts[1]))
-					if col == nil {
-						return
-					}
-				}
-			case 110:
-				col = defaultFg
-			case 111:
-				col = defaultBg
-			case 112:
-				col = defaultCur
+			if t.Title != nil {
+				t.Title(name)
 			}
-
-			switch cmd {
-			case 10, 110: // Set/Reset foreground color
-				setCol = t.SetForegroundColor
-			case 11, 111: // Set/Reset background color
-				setCol = t.SetBackgroundColor
-			case 12, 112: // Set/Reset cursor color
-				setCol = t.SetCursorColor
-			}
-
-			setCol(col)
 		}
+	case 10, 11, 12, 110, 111, 112:
+		var setCol func(color.Color)
+		var col color.Color
+
+		parts := bytes.Split(t.parser.Data(), []byte{';'})
+		if len(parts) == 0 {
+			// Invalid, ignore
+			return
+		}
+
+		switch cmd {
+		case 10, 11, 12:
+			if len(parts) != 2 {
+				// Invalid, ignore
+				return
+			}
+			if s := string(parts[1]); s == "?" {
+				switch cmd {
+				case 10:
+					col = t.ForegroundColor()
+				case 11:
+					col = t.BackgroundColor()
+				case 12:
+					col = t.CursorColor()
+				}
+
+				t.buf.WriteString(encodeOscColor(cmd.Command(), col))
+			} else {
+				col := xParseColor(string(parts[1]))
+				if col == nil {
+					return
+				}
+			}
+		case 110:
+			col = defaultFg
+		case 111:
+			col = defaultBg
+		case 112:
+			col = defaultCur
+		}
+
+		switch cmd {
+		case 10, 110: // Set/Reset foreground color
+			setCol = t.SetForegroundColor
+		case 11, 111: // Set/Reset background color
+			setCol = t.SetBackgroundColor
+		case 12, 112: // Set/Reset cursor color
+			setCol = t.SetCursorColor
+		}
+
+		setCol(col)
 	default:
-		t.logf("unhandled OSC: %d", seq.Command())
+		t.logf("unhandled OSC: %d", cmd)
 	}
 }
 
