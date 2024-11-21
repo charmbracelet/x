52210b910fa3c5b346d56273ce1d853fd610d4ae
Richard Wang <richardwa@google.com>
2024-11-22 01:12:04 +0900

    Add links

--- ansi/cwd.go
+++ ansi/cwd.go
@@ -11,6 +11,9 @@ import (
 //
 // Where Pt is a URL in the format "file://[host]/[path]".
 // Set host to "localhost" if this is a path on the local computer.
+//
+// See: https://wezfurlong.org/wezterm/shell-integration.html#osc-7-escape-sequence-to-set-the-working-directory
+// See: https://iterm2.com/documentation-escape-codes.html#:~:text=RemoteHost%20and%20CurrentDir%3A-,OSC%207,-%3B%20%5BPs%5D%20ST
 func NotifyWorkingDirectory(host string, path string) string {
 	u := &url.URL{
 		Scheme: "file",
