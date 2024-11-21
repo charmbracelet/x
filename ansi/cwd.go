a469e196fce13fb810555eb3d3f42b034a206db0
Richard Wang <richardwa@google.com>
2024-11-21 14:21:57 +0900

    feat(ansi): support OSC 7 notify working directory

---
+++ ansi/cwd.go
@@ -0,0 +1,21 @@
+package ansi
+
+import (
+	"net/url"
+)
+
+// NotifyWorkingDirectory returns a sequence for notifying the program's
+// current working directory.
+//
+//	OSC 7 ; Pt ; BEL
+//
+// Where Pt is a URL in the format "file://[host]/[path]".
+// Set host to "localhost" if this is a path on the local computer.
+func NotifyWorkingDirectory(host string, path string, params ...string) string {
+	u := &url.URL{
+		Scheme: "file",
+		Host:   host,
+		Path:   path,
+	}
+	return "\x1b]7;" + u.String() + "\x07"
+}
