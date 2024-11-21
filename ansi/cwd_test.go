7eb62c0599074ab46ac4a3e8860daae6c38f8e96
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-11-21 11:19:52 -0500

    Apply suggestions from code review

--- ansi/cwd_test.go
+++ ansi/cwd_test.go
@@ -7,14 +7,14 @@ import (
 )
 
 func TestNotifyWorkingDirectory_LocalFile(t *testing.T) {
-	h := ansi.NotifyWorkingDirectory("localhost", "/path/to/file")
+	h := ansi.NotifyWorkingDirectory("localhost", "path", "to", "file")
 	if h != "\x1b]7;file://localhost/path/to/file\x07" {
 		t.Errorf("Unexpected url: %s", h)
 	}
 }
 
 func TestNotifyWorkingDirectory_RemoteFile(t *testing.T) {
-	h := ansi.NotifyWorkingDirectory("example.com", "/path/to/file")
+	h := ansi.NotifyWorkingDirectory("example.com", "path", "to", "file")
 	if h != "\x1b]7;file://example.com/path/to/file\x07" {
 		t.Errorf("Unexpected url: %s", h)
 	}
