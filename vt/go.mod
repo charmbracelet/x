08d3e3b35b0daa7d46ea809004b2fed4e5717fe0
Ayman Bagabas <ayman.bagabas@gmail.com>
2024-11-21 11:39:38 -0500

    feat(vt): use ansi.XParseColor

--- vt/go.mod
+++ vt/go.mod
@@ -3,10 +3,12 @@ module github.com/charmbracelet/x/vt
 go 1.19
 
 require (
-	github.com/charmbracelet/x/ansi v0.5.0
+	github.com/charmbracelet/x/ansi v0.5.1
 	github.com/charmbracelet/x/wcwidth v0.0.0-20241011142426-46044092ad91
-	github.com/lucasb-eyer/go-colorful v1.2.0
 	github.com/rivo/uniseg v0.4.7
 )
 
-require golang.org/x/text v0.20.0 // indirect
+require (
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+)
--- vt/osc.go
+++ vt/osc.go
@@ -3,11 +3,8 @@ package vt
 import (
 	"bytes"
 	"image/color"
-	"strconv"
-	"strings"
 
 	"github.com/charmbracelet/x/ansi"
-	"github.com/lucasb-eyer/go-colorful"
 )
 
 // handleOsc handles an OSC escape sequence.
@@ -77,7 +74,7 @@ func (t *Terminal) handleOsc(seq ansi.OscSequence) {
 					t.buf.WriteString(enc(ansi.XRGBColorizer{Color: col}))
 				}
 			} else {
-				col := xParseColor(string(parts[1]))
+				col := ansi.XParseColor(string(parts[1]))
 				if col == nil {
 					return
 				}
@@ -103,51 +100,4 @@ func (t *Terminal) handleOsc(seq ansi.OscSequence) {
 	default:
 		t.logf("unhandled OSC: %s", seq)
 	}
-}
-
-type shiftable interface {
-	~uint | ~uint16 | ~uint32 | ~uint64
-}
-
-func shift[T shiftable](x T) T {
-	if x > 0xff {
-		x >>= 8
-	}
-	return x
-}
-
-func xParseColor(s string) color.Color {
-	switch {
-	case strings.HasPrefix(s, "#"):
-		c, err := colorful.Hex(s)
-		if err != nil {
-			return nil
-		}
-
-		return c
-	case strings.HasPrefix(s, "rgb:"):
-		parts := strings.Split(s[4:], "/")
-		if len(parts) != 3 {
-			return nil
-		}
-
-		r, _ := strconv.ParseUint(parts[0], 16, 32)
-		g, _ := strconv.ParseUint(parts[1], 16, 32)
-		b, _ := strconv.ParseUint(parts[2], 16, 32)
-
-		return color.RGBA{uint8(shift(r)), uint8(shift(g)), uint8(shift(b)), 255} //nolint:gosec
-	case strings.HasPrefix(s, "rgba:"):
-		parts := strings.Split(s[5:], "/")
-		if len(parts) != 4 {
-			return nil
-		}
-
-		r, _ := strconv.ParseUint(parts[0], 16, 32)
-		g, _ := strconv.ParseUint(parts[1], 16, 32)
-		b, _ := strconv.ParseUint(parts[2], 16, 32)
-		a, _ := strconv.ParseUint(parts[3], 16, 32)
-
-		return color.RGBA{uint8(shift(r)), uint8(shift(g)), uint8(shift(b)), uint8(shift(a))} //nolint:gosec
-	}
-	return nil
 }
