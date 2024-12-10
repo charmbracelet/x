e41b682f1dfb71791cc0ce375309fecb019a9571
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2024-12-10 13:24:06 -0300

    feat(ansi): TruncateLeft (#301)
    Truncate returns the first N chars of the string.
    TruncateLeft skips the first N chars of the string, returning the rest.

--- ansi/truncate.go
+++ ansi/truncate.go
@@ -105,3 +105,81 @@ func Truncate(s string, length int, tail string) string {
 
 	return buf.String()
 }
+
+// TruncateLeft truncates a string from the left side to a given length, adding
+// a prefix to the beginning if the string is longer than the given length.
+// This function is aware of ANSI escape codes and will not break them, and
+// accounts for wide-characters (such as East Asians and emojis).
+func TruncateLeft(s string, length int, prefix string) string {
+	if length == 0 {
+		return ""
+	}
+
+	var cluster []byte
+	var buf bytes.Buffer
+	curWidth := 0
+	ignoring := true
+	pstate := parser.GroundState
+	b := []byte(s)
+	i := 0
+
+	for i < len(b) {
+		if !ignoring {
+			buf.Write(b[i:])
+			break
+		}
+
+		state, action := parser.Table.Transition(pstate, b[i])
+		if state == parser.Utf8State {
+			var width int
+			cluster, _, width, _ = uniseg.FirstGraphemeCluster(b[i:], -1)
+
+			i += len(cluster)
+			curWidth += width
+
+			if curWidth > length && ignoring {
+				ignoring = false
+				buf.WriteString(prefix)
+			}
+
+			if ignoring {
+				continue
+			}
+
+			if curWidth > length {
+				buf.Write(cluster)
+			}
+
+			pstate = parser.GroundState
+			continue
+		}
+
+		switch action {
+		case parser.PrintAction:
+			curWidth++
+
+			if curWidth > length && ignoring {
+				ignoring = false
+				buf.WriteString(prefix)
+			}
+
+			if ignoring {
+				i++
+				continue
+			}
+
+			fallthrough
+		default:
+			buf.WriteByte(b[i])
+			i++
+		}
+
+		pstate = state
+		if curWidth > length && ignoring {
+			ignoring = false
+			buf.WriteString(prefix)
+		}
+	}
+
+	return buf.String()
+}
