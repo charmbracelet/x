charmbracelet/x

commit cbdcc21a06e3557256bd55b335b4dc303a7553c8

fix(cellbuf): SetContent should truncate the string if it doesn't fit in the window

cellbuf/window.go
@@ -154,7 +154,7 @@ func (c *Window) SetContent(s string) {
 	s = strings.ReplaceAll(s, "\n", "\r\n")
 	c.Clear()
 	c.MoveTo(0, 0)
-	c.Print(s)
+	c.PrintTruncate(s, "")
 }
 
 // Fill fills the window with the given cell and resets the cursor position,
@@ -275,7 +275,7 @@ func (c *Window) drawString(s string, x, y int, opts *drawOpts) {
 				cell = newGraphemeCell(seq, width)
 			}
 
-			if !opts.truncate && x >= c.w {
+			if !opts.truncate && x+width > c.x+c.w {
 				// Auto wrap the cursor.
 				wrapCursor()
 				if y >= c.h {

commit 48b574af22a51f5e56f1a5b3ae743a1ef2b5d4d8

fix(cellbuf): wrap when the cursor is at the end of the window
Fixes: cbdcc21a06e3 (fix(cellbuf): SetContent should truncate the string if it doesn't fit in the window)

cellbuf/window.go
@@ -275,7 +275,7 @@ func (c *Window) drawString(s string, x, y int, opts *drawOpts) {
 				cell = newGraphemeCell(seq, width)
 			}
 
-			if !opts.truncate && x+width > c.x+c.w {
+			if !opts.truncate && x >= c.w {
 				// Auto wrap the cursor.
 				wrapCursor()
 				if y >= c.h {