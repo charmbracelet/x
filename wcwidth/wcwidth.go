e328725af628cab6a96657fc2b4740c281331ef9
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-01-25 17:55:36 -0500

    refactor(wcwidth): deprecate wcwidth and use runewidth directly

--- wcwidth/wcwidth.go
+++ wcwidth/wcwidth.go
@@ -1,50 +1,21 @@
-// wcwidth is a Go implementation of wcwidth(3) that uses
-// golang.org/x/text/width. It is a drop-in replacement for
-// github.com/mattn/go-runewidth.
-//
-// Unlike go-runewidth, wcwidth treats East Asian ambiguous characters as
-// single-width characters. This is consistent with the behavior of wcwidth(3).
-//
-// See https://www.cl.cam.ac.uk/~mgk25/ucs/wcwidth.c
-
 package wcwidth
 
 import (
-	"unicode"
-
-	"golang.org/x/text/width"
+	"github.com/mattn/go-runewidth"
 )
 
-const nbsp = 0xA0
-
 // RuneWidth returns fixed-width width of rune.
-// https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms#In_Unicode
+//
+// Deprecated: this is now a wrapper around go-runewidth. Use go-runewidth
+// directly.
 func RuneWidth(r rune) int {
-	// No width for categories Me (Mark, enclosing), Mn (Mark, non-spacing), and
-	// Cf (Other, format). We treat Control characters (class Cc) as zero width
-	// instead of -1.
-	if r == 0 || !unicode.IsPrint(r) || unicode.In(r, unicode.Me, unicode.Mn, unicode.Cf) {
-		if r == nbsp { // Special case: non-breaking space has width 1
-			return 1
-		}
-		return 0
-	}
-	k := width.LookupRune(r)
-	switch k.Kind() {
-	case width.EastAsianWide, width.EastAsianFullwidth:
-		return 2
-	case width.EastAsianNarrow, width.EastAsianHalfwidth, width.EastAsianAmbiguous, width.Neutral:
-		return 1
-	default:
-		return 0
-	}
+	return runewidth.RuneWidth(r)
 }
 
 // StringWidth returns fixed-width width of string.
-// https://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms#In_Unicode
+//
+// Deprecated: this is now a wrapper around go-runewidth. Use go-runewidth
+// directly.
 func StringWidth(s string) (n int) {
-	for _, r := range s {
-		n += RuneWidth(r)
-	}
-	return
+	return runewidth.StringWidth(s)
 }
