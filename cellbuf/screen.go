charmbracelet/x

commit 914b36e9d09e2437de5f549e1fe1667fd53cbbdf

fix(cellbuf): ensure we're using the correct move method
The moveCursor method doesn't check if the cursor is out of the screen
in the phantom state, so we should use the move method instead.

cellbuf/screen.go
@@ -1075,11 +1075,11 @@ func (s *Screen) clearBottom(total int, force bool) (top int) {
 		}
 
 		if force || top < total {
-			s.moveCursor(0, top, false)
+			s.move(0, top)
 			s.clearToBottom(blank)
 			if !s.opts.AltScreen {
 				// Move to the last line of the screen
-				s.moveCursor(0, s.newbuf.Height()-1, false)
+				s.move(0, s.newbuf.Height()-1)
 			}
 			if s.oldhash != nil && s.newhash != nil &&
 				row < len(s.oldhash) && row < len(s.newhash) {
@@ -1105,7 +1105,7 @@ func (s *Screen) clearScreen(blank *Cell) {
 // clearBelow clears everything below the screen.
 func (s *Screen) clearBelow(blank *Cell, row int) {
 	s.updatePen(blank)
-	s.moveCursor(0, row, false)
+	s.move(0, row)
 	s.clearToBottom(blank)
 	s.cur.X, s.cur.Y = 0, row
 	s.curbuf.FillRect(blank, Rect(0, row, s.curbuf.Width(), s.curbuf.Height()))
@@ -1188,12 +1188,12 @@ func (s *Screen) render() {
 		// We need to scroll the screen up by the number of lines in the queue.
 		// We can't use [ansi.SU] because we want the cursor to move down until
 		// it reaches the bottom of the screen.
-		s.moveCursor(0, s.newbuf.Height()-1, false)
+		s.move(0, s.newbuf.Height()-1)
 		s.buf.WriteString(strings.Repeat("\n", len(s.queueAbove)))
 		s.cur.Y += len(s.queueAbove)
 		// Now go to the top of the screen, insert new lines, and write the
 		// queued strings.
-		s.moveCursor(0, 0, false)
+		s.move(0, 0)
 		s.buf.WriteString(ansi.InsertLine(len(s.queueAbove)))
 		for _, line := range s.queueAbove {
 			s.buf.WriteString(line + "\r\n")

commit 7979dda07a20b5c68887f1da6d2e58bd57eb5026

fix(cellbuf): off by one error when moving the cursor in inline mode
Always use Screen.move instead of Screen.moveCursor when moving the cursor
to perform out of screen phantom state and wrapping checks. One
exception is when we print above the screen and move the cursor to the
top-left corner of the inline screen.

Fixes: 914b36e9d09e (fix(cellbuf): ensure we're using the correct move method)

cellbuf/screen.go
@@ -246,7 +246,11 @@ func (s *Screen) moveCursor(x, y int, overwrite bool) {
 }
 
 func (s *Screen) move(x, y int) {
-	width, height := s.newbuf.Width(), s.newbuf.Height()
+	// XXX: Make sure we use the max height and width of the buffer in case
+	// we're in the middle of a resize operation.
+	width := max(s.newbuf.Width(), s.curbuf.Width())
+	height := max(s.newbuf.Height(), s.curbuf.Height())
+
 	if width > 0 && x >= width {
 		// Handle autowrap
 		y += (x / width)
@@ -1191,9 +1195,11 @@ func (s *Screen) render() {
 		s.move(0, s.newbuf.Height()-1)
 		s.buf.WriteString(strings.Repeat("\n", len(s.queueAbove)))
 		s.cur.Y += len(s.queueAbove)
-		// Now go to the top of the screen, insert new lines, and write the
-		// queued strings.
-		s.move(0, 0)
+		// XXX: Now go to the top of the screen, insert new lines, and write
+		// the queued strings. It is important to use [Screen.moveCursor]
+		// instead of [Screen.move] because we don't want to perform any checks
+		// on the cursor position.
+		s.moveCursor(0, 0, false)
 		s.buf.WriteString(ansi.InsertLine(len(s.queueAbove)))
 		for _, line := range s.queueAbove {
 			s.buf.WriteString(line + "\r\n")