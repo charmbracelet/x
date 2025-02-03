charmbracelet/x

commit 7979dda07a20b5c68887f1da6d2e58bd57eb5026

fix(cellbuf): off by one error when moving the cursor in inline mode
Always use Screen.move instead of Screen.moveCursor when moving the cursor
to perform out of screen phantom state and wrapping checks. One
exception is when we print above the screen and move the cursor to the
top-left corner of the inline screen.

Fixes: 914b36e9d09e (fix(cellbuf): ensure we're using the correct move method)

cellbuf/screen.go
@@ -246,7 +246,11 @@ func (s *Screen) moveCursor(x, y int, overwrite bool) {
 }
 
 func (s *Screen) move(x, y int) {
-	width, height := s.newbuf.Width(), s.newbuf.Height()
+	// XXX: Make sure we use the max height and width of the buffer in case
+	// we're in the middle of a resize operation.
+	width := max(s.newbuf.Width(), s.curbuf.Width())
+	height := max(s.newbuf.Height(), s.curbuf.Height())
+
 	if width > 0 && x >= width {
 		// Handle autowrap
 		y += (x / width)
@@ -1191,9 +1195,11 @@ func (s *Screen) render() {
 		s.move(0, s.newbuf.Height()-1)
 		s.buf.WriteString(strings.Repeat("\n", len(s.queueAbove)))
 		s.cur.Y += len(s.queueAbove)
-		// Now go to the top of the screen, insert new lines, and write the
-		// queued strings.
-		s.move(0, 0)
+		// XXX: Now go to the top of the screen, insert new lines, and write
+		// the queued strings. It is important to use [Screen.moveCursor]
+		// instead of [Screen.move] because we don't want to perform any checks
+		// on the cursor position.
+		s.moveCursor(0, 0, false)
 		s.buf.WriteString(ansi.InsertLine(len(s.queueAbove)))
 		for _, line := range s.queueAbove {
 			s.buf.WriteString(line + "\r\n")

commit dae086958a3e6029ce3a66696ba2b7bd94a1645e

fix(cellbuf): properly handle autowrap
Only set cursor position when wrapping before writing the cell.

Fixes: 7979dda07a20 (fix(cellbuf): off by one error when moving the cursor in inline mode)

cellbuf/screen.go
@@ -559,16 +559,18 @@ func (s *Screen) putCell(cell *Cell) {
 	} else {
 		s.putAttrCell(cell)
 	}
-
-	if s.cur.X >= width {
-		s.wrapCursor()
-	}
 }
 
 // wrapCursor wraps the cursor to the next line.
 func (s *Screen) wrapCursor() {
-	s.cur.X = 0
-	s.cur.Y++
+	const autoRightMargin = true
+	if autoRightMargin {
+		// Assume we have auto wrap mode enabled.
+		s.cur.X = 0
+		s.cur.Y++
+	} else {
+		s.cur.X--
+	}
 }
 
 func (s *Screen) putAttrCell(cell *Cell) {
@@ -580,10 +582,11 @@ func (s *Screen) putAttrCell(cell *Cell) {
 		cell = s.clearBlank()
 	}
 
-	// if s.cur.X >= s.newbuf.Width() {
-	// 	// TODO: Properly handle autowrap.
-	// 	s.wrapCursor()
-	// }
+	if cell.Width > 0 && s.cur.X >= s.newbuf.Width() {
+		// We're at pending wrap state (phantom cell), incoming cell should
+		// wrap.
+		s.wrapCursor()
+	}
 
 	s.updatePen(cell)
 	s.buf.WriteRune(cell.Rune) //nolint:errcheck
@@ -593,11 +596,6 @@ func (s *Screen) putAttrCell(cell *Cell) {
 	s.cur.X += cell.Width
 	if cell.Width > 0 {
 		s.queuedText = true
-	}
-
-	if s.cur.X >= s.newbuf.Width() {
-		// TODO: Properly handle autowrap. This is a hack.
-		s.cur.X = s.newbuf.Width() - 1
 	}
 }
 