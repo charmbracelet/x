charmbracelet/x

commit dae086958a3e6029ce3a66696ba2b7bd94a1645e

fix(cellbuf): properly handle autowrap
Only set cursor position when wrapping before writing the cell.

Fixes: 7979dda07a20 (fix(cellbuf): off by one error when moving the cursor in inline mode)

cellbuf/screen.go
@@ -559,16 +559,18 @@ func (s *Screen) putCell(cell *Cell) {
 	} else {
 		s.putAttrCell(cell)
 	}
-
-	if s.cur.X >= width {
-		s.wrapCursor()
-	}
 }
 
 // wrapCursor wraps the cursor to the next line.
 func (s *Screen) wrapCursor() {
-	s.cur.X = 0
-	s.cur.Y++
+	const autoRightMargin = true
+	if autoRightMargin {
+		// Assume we have auto wrap mode enabled.
+		s.cur.X = 0
+		s.cur.Y++
+	} else {
+		s.cur.X--
+	}
 }
 
 func (s *Screen) putAttrCell(cell *Cell) {
@@ -580,10 +582,11 @@ func (s *Screen) putAttrCell(cell *Cell) {
 		cell = s.clearBlank()
 	}
 
-	// if s.cur.X >= s.newbuf.Width() {
-	// 	// TODO: Properly handle autowrap.
-	// 	s.wrapCursor()
-	// }
+	if cell.Width > 0 && s.cur.X >= s.newbuf.Width() {
+		// We're at pending wrap state (phantom cell), incoming cell should
+		// wrap.
+		s.wrapCursor()
+	}
 
 	s.updatePen(cell)
 	s.buf.WriteRune(cell.Rune) //nolint:errcheck
@@ -593,11 +596,6 @@ func (s *Screen) putAttrCell(cell *Cell) {
 	s.cur.X += cell.Width
 	if cell.Width > 0 {
 		s.queuedText = true
-	}
-
-	if s.cur.X >= s.newbuf.Width() {
-		// TODO: Properly handle autowrap. This is a hack.
-		s.cur.X = s.newbuf.Width() - 1
 	}
 }
 

commit dee611fb8adb28b9587013e1a4903646558e6ab1

fix(cellbuf): restore previous autowrap behavior
Fixes: dae086958a3e (fix(cellbuf): properly handle autowrap)

cellbuf/screen.go
@@ -562,6 +562,8 @@ func (s *Screen) putCell(cell *Cell) {
 }
 
 // wrapCursor wraps the cursor to the next line.
+//
+//nolint:unused
 func (s *Screen) wrapCursor() {
 	const autoRightMargin = true
 	if autoRightMargin {
@@ -582,11 +584,11 @@ func (s *Screen) putAttrCell(cell *Cell) {
 		cell = s.clearBlank()
 	}
 
-	if cell.Width > 0 && s.cur.X >= s.newbuf.Width() {
-		// We're at pending wrap state (phantom cell), incoming cell should
-		// wrap.
-		s.wrapCursor()
-	}
+	// if cell.Width > 0 && s.cur.X >= s.newbuf.Width() {
+	// 	// We're at pending wrap state (phantom cell), incoming cell should
+	// 	// wrap.
+	// 	s.wrapCursor()
+	// }
 
 	s.updatePen(cell)
 	s.buf.WriteRune(cell.Rune) //nolint:errcheck
@@ -596,6 +598,13 @@ func (s *Screen) putAttrCell(cell *Cell) {
 	s.cur.X += cell.Width
 	if cell.Width > 0 {
 		s.queuedText = true
+	}
+
+	if s.cur.X >= s.newbuf.Width() {
+		// TODO: Properly handle autowrap, this is probably wrong.
+		// See commented code above and commit dae086958a3e.
+		// s.wrapCursor()
+		s.cur.X = s.newbuf.Width() - 1
 	}
 }
 