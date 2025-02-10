charmbracelet/x

commit f7cbc7fdbe3419416f5567aa9263391997cc3a2a

fix(cellbuf): reset styles before moving the cursor

cellbuf/screen.go
@@ -261,14 +261,15 @@ func (s *Screen) move(x, y int) {
 		x %= width
 	}
 
-	// Disable styles if there's any
-	// TODO: Do we need this? It seems like it's only needed when used with
-	// alternate character sets which we don't support.
-	// var pen Style
-	// if !s.cur.Style.Empty() {
-	// 	pen = s.cur.Style
-	// 	s.buf.WriteString(ansi.ResetStyle) //nolint:errcheck
-	// }
+	// XXX: Disable styles if there's any
+	// Some move operations such as [ansi.LF] can apply styles to the new
+	// cursor position, thus, we need to reset the styles before moving the
+	// cursor.
+	var pen Style
+	if !s.cur.Style.Empty() {
+		pen = s.cur.Style
+		s.buf.WriteString(ansi.ResetStyle) //nolint:errcheck
+	}
 
 	// Reset wrap around (phantom cursor) state
 	if s.atPhantom {
@@ -312,11 +313,10 @@ func (s *Screen) move(x, y int) {
 	// We set the new cursor in [Screen.moveCursor].
 	s.moveCursor(x, y, true) // Overwrite cells if possible
 
-	// TODO: Do we need this? It seems like it's only needed when used with
-	// alternate character sets which we don't support.
-	// if !pen.Empty() {
-	// 	s.buf.WriteString(pen.Sequence()) //nolint:errcheck
-	// }
+	// Restore pen styles before moving the cursor.
+	if !pen.Empty() {
+		s.buf.WriteString(pen.Sequence()) //nolint:errcheck
+	}
 }
 
 // Cursor represents a terminal Cursor.

commit ae6ce28c9759ab2f8bbe6f3c7bc8a3ef3095e36d

fix(cellbuf): only reset styles when moving the cursor to a new line
Fixes: f7cbc7fdbe34 (fix(cellbuf): reset styles before moving the cursor)

cellbuf/screen.go
@@ -266,10 +266,10 @@ func (s *Screen) move(x, y int) {
 	// Some move operations such as [ansi.LF] can apply styles to the new
 	// cursor position, thus, we need to reset the styles before moving the
 	// cursor.
-	var pen Style
-	if !s.cur.Style.Empty() {
-		pen = s.cur.Style
-		s.buf.WriteString(ansi.ResetStyle) //nolint:errcheck
+	blank := s.clearBlank()
+	resetPen := y != s.cur.Y && !blank.Equal(&BlankCell)
+	if resetPen {
+		s.updatePen(nil)
 	}
 
 	// Reset wrap around (phantom cursor) state
@@ -315,8 +315,8 @@ func (s *Screen) move(x, y int) {
 	s.moveCursor(x, y, true) // Overwrite cells if possible
 
 	// Restore pen styles before moving the cursor.
-	if !pen.Empty() {
-		s.buf.WriteString(pen.Sequence()) //nolint:errcheck
+	if resetPen {
+		s.updatePen(blank)
 	}
 }
 