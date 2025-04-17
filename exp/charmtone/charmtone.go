7ab098639b3c2a2e07e08aae1788e1dd554beede
Christian Rocha <christian@rocha.is>
2025-04-16 21:46:18 -0400

    feat(charmtone): initial pass

---
+++ exp/charmtone/charmtone.go
@@ -0,0 +1,132 @@
+package charmtone
+
+import "github.com/charmbracelet/lipgloss/v2"
+
+const (
+	cumin     = "#BF976F"
+	tang      = "#FF985A" // *
+	yam       = "#FFB587" // *
+	paprika   = "#D36C64"
+	pumpkin   = "#FF6E63"
+	uni       = "#FF937D" // *
+	sriracha  = "#EB4268" // *
+	coral     = "#FF577D" // *
+	salmon    = "#FF7F90"
+	chili     = "#E23080"
+	cherry    = "#FF388B"
+	tuna      = "#FF6DAA"
+	macaroon  = "#E940B0"
+	rose      = "#FF4FBF"
+	cheeky    = "#FF79D0"
+	flamingo  = "#F947E3"
+	mollie    = "#FF60FF"
+	blush     = "#FF84FF"
+	urchin    = "#C337E0"
+	crystal   = "#EB5DFF"
+	lilac     = "#F379FF"
+	eggplant  = "#9C35E1"
+	violet    = "#C259FF"
+	mauve     = "#D46EFF"
+	grape     = "#7134DD"
+	plum      = "#9953FF"
+	orchid    = "#AD6EFF"
+	fig       = "#4A30D9"
+	charple   = "#6B50FF"
+	hazy      = "#8B75FF"
+	blueberry = "#3331B2"
+	sapphire  = "#4949FF"
+	guppy     = "#7272FF" // *
+	neptune   = "#2B55B3"
+	electric  = "#4776FF"
+	anchovy   = "#719AFC"
+	damson    = "#007AB8"
+	malibu    = "#00A4FF"
+	sardine   = "#4FBEFE"
+	zinc      = "#10B1AE"
+	turtle    = "#0ADCD9"
+	lichen    = "#5CDFEA"
+	guac      = "#14DD9F"
+	julep     = "#00FFB2"
+	bok       = "#68FFD6"
+	mustard   = "#F5EF34"
+	melon     = "#E8FF27"
+	zest      = "#E8FE96"
+
+	pepper   = "#201F26"
+	charcoal = "#3A3943"
+	oyster   = "#605F6B"
+	squid    = "#858392"
+	smoke    = "#С0ВСС9"
+	butter   = "#FFFAF1"
+
+	// Gray aliases
+	gray1 = pepper
+	gray2 = charcoal
+	gray3 = oyster
+	gray4 = squid
+	gray5 = smoke
+)
+
+var Charmtone = map[string]string{
+	"cumin":     lipgloss.Color(cumin),
+	"tang":      lipgloss.Color(tang),
+	"yam":       lipgloss.Color(yam),
+	"paprika":   lipgloss.Color(paprika),
+	"pumpkin":   lipgloss.Color(pumpkin),
+	"uni":       lipgloss.Color(uni),
+	"sriracha":  lipgloss.Color(sriracha),
+	"coral":     lipgloss.Color(coral),
+	"salmon":    lipgloss.Color(salmon),
+	"chili":     lipgloss.Color(chili),
+	"cherry":    lipgloss.Color(cherry),
+	"tuna":      lipgloss.Color(tuna),
+	"macaroon":  lipgloss.Color(macaroon),
+	"rose":      lipgloss.Color(rose),
+	"cheeky":    lipgloss.Color(cheeky),
+	"flamingo":  lipgloss.Color(flamingo),
+	"mollie":    lipgloss.Color(mollie),
+	"blush":     lipgloss.Color(blush),
+	"urchin":    lipgloss.Color(urchin),
+	"crystal":   lipgloss.Color(crystal),
+	"lilac":     lipgloss.Color(lilac),
+	"eggplant":  lipgloss.Color(eggplant),
+	"violet":    lipgloss.Color(violet),
+	"mauve":     lipgloss.Color(mauve),
+	"grape":     lipgloss.Color(grape),
+	"plum":      lipgloss.Color(plum),
+	"orchid":    lipgloss.Color(orchid),
+	"fig":       lipgloss.Color(fig),
+	"charple":   lipgloss.Color(charple),
+	"hazy":      lipgloss.Color(hazy),
+	"blueberry": lipgloss.Color(blueberry),
+	"sapphire":  lipgloss.Color(sapphire),
+	"guppy":     lipgloss.Color(guppy),
+	"neptune":   lipgloss.Color(neptune),
+	"electric":  lipgloss.Color(electric),
+	"anchovy":   lipgloss.Color(anchovy),
+	"damson":    lipgloss.Color(damson),
+	"malibu":    lipgloss.Color(malibu),
+	"sardine":   lipgloss.Color(sardine),
+	"zinc":      lipgloss.Color(zinc),
+	"turtle":    lipgloss.Color(turtle),
+	"lichen":    lipgloss.Color(lichen),
+	"guac":      lipgloss.Color(guac),
+	"julep":     lipgloss.Color(julep),
+	"bok":       lipgloss.Color(bok),
+	"mustard":   lipgloss.Color(mustard),
+	"melon":     lipgloss.Color(melon),
+	"zest":      lipgloss.Color(zest),
+
+	"pepper":   lipgloss.Color(pepper),
+	"charcoal": lipgloss.Color(charcoal),
+	"oyster":   lipgloss.Color(oyster),
+	"squid":    lipgloss.Color(squid),
+	"smoke":    lipgloss.Color(smoke),
+	"butter":   lipgloss.Color(butter),
+
+	"gray1": lipgloss.Color(gray1),
+	"gray2": lipgloss.Color(gray2),
+	"gray3": lipgloss.Color(gray3),
+	"gray4": lipgloss.Color(gray4),
+	"gray5": lipgloss.Color(gray5),
+}
