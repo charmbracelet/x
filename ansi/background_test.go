fb931ed9041392fb61ea5ae93a3408d157f50440
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-06-02 11:29:26 -0400

    fix(ansi): remove Colorizer and simplify color handling using strings
    The implementation of bg/fg/cursor color handling has been simplified by
    removing the color.Color interfaces and simply using strings. A color
    needs to be formatted to a specific string format and the implementer
    can choose whatever string format supported by the terminal.

--- ansi/background_test.go
+++ ansi/background_test.go
@@ -4,19 +4,29 @@ import (
 	"testing"
 
 	"github.com/charmbracelet/x/ansi"
+	"github.com/lucasb-eyer/go-colorful"
 )
 
 func TestSetForegroundColorNil(t *testing.T) {
-	s := ansi.SetForegroundColor(nil)
+	s := ansi.SetForegroundColor("")
 	if s != "\x1b]10;\x07" {
 		t.Errorf("Unexpected string for SetForegroundColor: got %q", s)
 	}
 }
 
 func TestStringImplementations(t *testing.T) {
-	foregroundColor := ansi.SetForegroundColor(ansi.BrightMagenta)
-	backgroundColor := ansi.SetBackgroundColor(ansi.ExtendedColor(255))
-	cursorColor := ansi.SetCursorColor(ansi.TrueColor(0xffeeaa))
+	brightMagenta, ok := colorful.MakeColor(ansi.BrightMagenta)
+	if !ok {
+		t.Fatalf("Failed to create color for BrightMagenta: %v", ansi.BrightMagenta)
+	}
+	color255, ok := colorful.MakeColor(ansi.ExtendedColor(255))
+	if !ok {
+		t.Fatalf("Failed to create color for ExtendedColor(255): %v", ansi.ExtendedColor(255))
+	}
+	hexColor := ansi.HexColor("#ffeeaa")
+	foregroundColor := ansi.SetForegroundColor(brightMagenta.Hex())
+	backgroundColor := ansi.SetBackgroundColor(color255.Hex())
+	cursorColor := ansi.SetCursorColor(hexColor.Hex())
 
 	if foregroundColor != "\x1b]10;#ff00ff\x07" {
 		t.Errorf("Unexpected string for SetForegroundColor: got %q",
@@ -29,21 +39,5 @@ func TestStringImplementations(t *testing.T) {
 	if cursorColor != "\x1b]12;#ffeeaa\x07" {
 		t.Errorf("Unexpected string for SetCursorColor: got %q",
 			cursorColor)
-	}
-}
-
-func TestColorizer(t *testing.T) {
-	hex := ansi.HexColorizer{ansi.BrightBlack}
-	xrgb := ansi.XRGBColorizer{ansi.ExtendedColor(235)}
-	xrgba := ansi.XRGBAColorizer{ansi.TrueColor(0x00ff00)}
-
-	if seq := ansi.SetForegroundColor(hex); seq != "\x1b]10;#808080\x07" {
-		t.Errorf("Unexpected sequence for HexColorizer: got %q", seq)
-	}
-	if seq := ansi.SetForegroundColor(xrgb); seq != "\x1b]10;rgb:2626/2626/2626\x07" {
-		t.Errorf("Unexpected sequence for XRGBColorizer: got %q", seq)
-	}
-	if seq := ansi.SetForegroundColor(xrgba); seq != "\x1b]10;rgba:0000/ffff/0000/ffff\x07" {
-		t.Errorf("Unexpected sequence for XRGBAColorizer: got %q", seq)
 	}
 }
