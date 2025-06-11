a99057a35895eec2b79aa212df8c3906bfe31bcd
Ayman Bagabas <ayman.bagabas@gmail.com>
2025-06-11 11:22:12 -0400

    fix(vt): validate eraseCharacter argument

--- vt/csi_screen.go
+++ vt/csi_screen.go
@@ -5,6 +5,9 @@ import "github.com/charmbracelet/uv"
 // eraseCharacter erases n characters starting from the cursor position. It
 // does not move the cursor. This is equivalent to [ansi.ECH].
 func (t *Terminal) eraseCharacter(n int) {
+	if n <= 0 {
+		n = 1
+	}
 	x, y := t.scr.CursorPosition()
 	rect := uv.Rect(x, y, n, 1)
 	t.scr.Fill(t.scr.blankCell(), rect)
