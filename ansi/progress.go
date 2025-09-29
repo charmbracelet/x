80cac83d019d58a3ed64810d401bc0fe509e5d75
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-09-29 16:45:36 -0300

    feat(ansi): osc 9 progress support (#574)
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

---
+++ ansi/progress.go
@@ -0,0 +1,39 @@
+package ansi
+
+import "strconv"
+
+// ResetProgress is a sequence that resets the progress bar to its default
+// state (hidden).
+//
+// OSC 9 ; 4 ; 0 BEL
+//
+// See: https://learn.microsoft.com/en-us/windows/terminal/tutorials/progress-bar-sequences
+const ResetProgress = "\x1b]9;4;0\x07"
+
+// SetProgress returns a sequence for setting the progress bar to a specific
+// percentage (0-100) in the "default" state.
+//
+// OSC 9 ; 4 ; 1 ; Percentage BEL
+//
+// See: https://learn.microsoft.com/en-us/windows/terminal/tutorials/progress-bar-sequences
+func SetProgress(percentage int) string {
+	return "\x1b]9;4;1;" + strconv.Itoa(percentage) + "\x07"
+}
+
+// SetErrorProgress returns a sequence for setting the progress bar to a specific
+// percentage (0-100) in the "Error" state..
+//
+// OSC 9 ; 4 ; 2 ; Percentage BEL
+//
+// See: https://learn.microsoft.com/en-us/windows/terminal/tutorials/progress-bar-sequences
+func SetErrorProgress(percentage int) string {
+	return "\x1b]9;4;2;" + strconv.Itoa(percentage) + "\x07"
+}
+
+// SetIntermitentProgress is a sequence that sets the progress bar to the
+// intermitent state.
+//
+// OSC 9 ; 4 ; 3 BEL
+//
+// See: https://learn.microsoft.com/en-us/windows/terminal/tutorials/progress-bar-sequences
+const SetIntermitentProgress = "\x1b]9;4;3\x07"
