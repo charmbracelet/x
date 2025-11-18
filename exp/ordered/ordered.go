77d017256798293400b6cdf263730e28e1a41091
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2025-11-18 14:27:36 -0300

    feat(ordered): remove Min & Max (#649)
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- exp/ordered/ordered.go
+++ exp/ordered/ordered.go
@@ -3,28 +3,12 @@ package ordered
 
 import "cmp"
 
-// Min returns the smaller of a and b.
-func Min[T cmp.Ordered](a, b T) T {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-// Max returns the larger of a and b.
-func Max[T cmp.Ordered](a, b T) T {
-	if a > b {
-		return a
-	}
-	return b
-}
-
 // Clamp returns a value clamped between the given low and high values.
 func Clamp[T cmp.Ordered](n, low, high T) T {
 	if low > high {
 		low, high = high, low
 	}
-	return Min(high, Max(low, n))
+	return min(high, max(low, n))
 }
 
 // First returns the first non-default value of a fixed number of
