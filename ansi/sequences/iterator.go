c828ff40712cff6c3447c912eb5d8e2362b0e345
Ayman Bagabas <ayman.bagabas@gmail.com>
2026-01-30 09:49:40 +0300

    fix(ansi): sequences: handle multiple sequences in FromString and FromBytes

--- ansi/sequences/iterator.go
+++ ansi/sequences/iterator.go
@@ -10,14 +10,16 @@ import (
 func FromString(s string) iter.Seq[string] {
 	var state state[string]
 	return func(yield func(string) bool) {
-		n, tok, err := state.splitFunc(s, len(s) == 0)
-		if err != nil {
-			panic(fmt.Sprintf("error in split function: %v", err))
+		for len(s) > 0 {
+			n, tok, err := state.splitFunc(s, len(s) == 0)
+			if err != nil {
+				panic(fmt.Sprintf("error in split function: %v", err))
+			}
+			if !yield(tok) {
+				return
+			}
+			s = s[n:]
 		}
-		if !yield(tok) {
-			return
-		}
-		s = s[n:]
 	}
 }
 
@@ -26,13 +28,15 @@ func FromString(s string) iter.Seq[string] {
 func FromBytes(b []byte) iter.Seq[[]byte] {
 	var state state[[]byte]
 	return func(yield func([]byte) bool) {
-		n, tok, err := state.splitFunc(b, len(b) == 0)
-		if err != nil {
-			panic(fmt.Sprintf("error in split function: %v", err))
+		for len(b) > 0 {
+			n, tok, err := state.splitFunc(b, len(b) == 0)
+			if err != nil {
+				panic(fmt.Sprintf("error in split function: %v", err))
+			}
+			if !yield(tok) {
+				return
+			}
+			b = b[n:]
 		}
-		if !yield(tok) {
-			return
-		}
-		b = b[n:]
 	}
 }
